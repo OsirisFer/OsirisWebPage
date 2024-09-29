2914a51f91b15a28c1dd86ae4908eeb7e39c27d2
OsirisFer <88387319+OsirisFer@users.noreply.github.com>
2024-09-29 16:15:39 -0300

    Update CSS styles for CheatSheetCss.css

--- Pruebas/PruebaPrueba.js
+++ Pruebas/PruebaPrueba.js
@@ -14,11 +14,3 @@ otroObjeto.edad = 30;
 otroObjeto.casado = false;
 
 
-function mostrarPagina(numero) {
-    var paginas = document.getElementsByClassName("pagina");
-    for (var i = 0; i < paginas.length; i++) {
-        paginas[i].style.display = "none";
-    }
-    document.getElementById("pagina" + numero).style.display = "block";
-}
-
